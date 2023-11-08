eff7db41e73b31c45fba0f8939fd8861ec7134e7
rynchng <150175907+rynchng@users.noreply.github.com>
2023-11-08 12:59:10 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,3 +1,20 @@
 public class Student{
   public String firstName;
+  public String favAnimal;
+
+  public Student(String name, String animal)
+  {
+    firstName = name;
+    favAnimal = animal;
+  }
+
+  public String getName()
+  {
+    return firstName;
+  }
+
+  public String getFavouriteAnimal()
+  {
+    return favAnimal;
+  }
 }
